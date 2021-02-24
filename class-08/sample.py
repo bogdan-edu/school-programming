b5c3e74149166def319ce6b653680a53088f3638
Bogdan Kalnybolotchuk <bogdan.education@gmail.com>
2021-02-24 13:09:21 +0200

    add file

---
+++ class-08/sample.py
@@ -0,0 +1,9 @@
+import random
+
+qube1 = random.randint(1, 10)
+qube2 = random.randint(1, 10)
+qube3 = random.randint(1, 10)
+points = qube1 + qube2 + qube3
+print('На кубику випало ', qube1, ' очок')
+print(qube2)
+print(qube3)
