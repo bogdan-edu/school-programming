6a7211379e4001a0697506cc1efaf6922288dbbd
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-03-12 14:01:39 +0200

    Modify

--- class-09/arrays/rand_array.py
+++ class-09/arrays/rand_array.py
@@ -1,7 +1,15 @@
 import random
 
+n = int(input('Скільки елементів масиву має бути? '))
+
 random_numbers = []
-for i in range(10):
+for i in range(n):
     random_numbers.append(random.randint(1, 10))
 
 print(random_numbers)
+
+summ = 0
+for i in range(len(random_numbers)):
+    summ = summ + random_numbers[i]
+
+print('Сума еелемнтів', summ)
