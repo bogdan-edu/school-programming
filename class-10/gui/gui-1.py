8b060266c551d82a8c5ee25f85656ef27bfeedcd
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-04-10 19:23:25 +0300

    Modify

--- class-10/gui/gui-1.py
+++ class-10/gui/gui-1.py
@@ -1,17 +1,21 @@
 from tkinter import *
+
 root = Tk()
 var1 = StringVar()
 var2 = StringVar()
 check1 = Checkbutton(root, text='One', variable=var1, onvalue='One ON', offvalue='One OFF')
 check2 = Checkbutton(root, text='Two', variable=var2, onvalue='Two ON', offvalue='Two OFF')
 list_all = Listbox(root, bg='yellow', height=3)
-def  check_worker(event):
+
+
+def check_worker(event):
     v1 = var1.get()
     v2 = var2.get()
     l = [v1, v2]
     list_all.delete(0, 1)
     for i in l:
         list_all.insert(END, i)
+
 
 btn = Button(root, text='Flags value')
 btn.bind('<Button-1>', check_worker)
