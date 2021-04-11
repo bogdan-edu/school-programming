0de2b6076d564aa5d81db9ed0fce78940916a6d1
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-04-12 01:56:08 +0300

    Modify

--- class-10/gui/gui-sample-3.py
+++ class-10/gui/gui-sample-3.py
@@ -4,10 +4,10 @@ root = Tk()
 var_a = DoubleVar()
 var_b = DoubleVar()
 
-ent_a = Entry(root, width=20, bd=3, text=var_a)
-ent_b = Entry(root, width=20, bd=3, text=var_b)
+ent_a = Entry(root, width=40, bd=5, font='Hack', text=var_a)
+ent_b = Entry(root, width=40, bd=5, font='Hack', text=var_b)
 
-res = Label(root)
+res = Label(root, height=2, width=40, font='Hack', border=5)
 
 
 def summary(event):
@@ -16,11 +16,11 @@ def summary(event):
     res['text'] = str(a + b)
 
 
-button = Button(root)
+button = Button(root, width=40, height=2, font='Hack', border=5)
 button['text'] = 'Додавання'
 button.bind('<Button>', summary)
+res.pack()
 ent_a.pack()
 ent_b.pack()
-res.pack()
 button.pack()
 root.mainloop()
