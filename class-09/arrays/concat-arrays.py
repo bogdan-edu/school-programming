fc0bfb92e36eeecd4bc8df87ad41da30f86fa55f
Bogdan Kalnybolotchuk <bogdan.education@gmail.com>
2021-04-13 13:16:14 +0300

    Addd

---
+++ class-09/arrays/concat-arrays.py
@@ -0,0 +1,11 @@
+a = [15, 12, 1, 0, 8, 555, 45, 101, 222, 1000]
+b = [1, 22, 333, 4444, 55555]
+
+c = []
+for i in range(len(a)):
+    c.append(a[i])
+
+for i in range(len(b)):
+    c.append(b[i])
+
+print(c)
