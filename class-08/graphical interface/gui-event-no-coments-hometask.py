e5ce5d18fe4070a4e437d3177970ea30fe336f8d
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-04-26 14:01:21 +0300

    new files

--- class-08/graphical interface/gui-event-no-coments-hometask.py
+++ class-08/graphical interface/gui-event-no-coments-hometask.py
@@ -5,12 +5,19 @@ def press_btn(event):
     my_text['text'] = 'Молодець, в тебе все вийде!'
 
 
+# Додаємо функцію обробник для другої кнопки
+def my_function(event):
+    my_text['text'] = 'Молодець, в тебе все вийшло!'
+
+
 root = Tk()
 my_text = Label(root, text='Привіт', width=50, height=3)
 btn = Button(root, text='Натисни мене')
 # Додаємо кнопку
 my_btn = Button(root, text='Моя кнопка')
 btn.bind('<Button>', press_btn)
+# Пов'язуємо обробник my_function з подією натискання нової кнопки
+my_btn.bind('<Button>', my_function)
 my_text.pack()
 btn.pack()
 # Застосовуємо метод pack до нової кнопки
