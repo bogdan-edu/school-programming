a7485ef851ab05ad7d1b99bd215598802ecf131f
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-04-26 13:53:29 +0300

    new files

--- class-08/graphical interface/gui-event-no-coments-hometask.py
+++ class-08/graphical interface/gui-event-no-coments-hometask.py
@@ -8,6 +8,8 @@ def press_btn(event):
 root = Tk()
 my_text = Label(root, text='Привіт', width=50, height=3)
 btn = Button(root, text='Натисни мене')
+# Додаємо кнопку
+my_btn = Button(root, text='Моя кнопка')
 btn.bind('<Button>', press_btn)
 my_text.pack()
 btn.pack()
