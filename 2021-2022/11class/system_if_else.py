b4171eb4ec8651b6d62476de4c07ae84f22c37e5
bogdan-edu <bogdan-education@gmail.com>
2021-10-06 10:51:41 +0300

    upd

---
+++ 2021-2022/11class/system_if_else.py
@@ -0,0 +1,8 @@
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+
+if a == 0:
+    print(f" y = {(a ** 3 + b ** 2) * c - 1}")
+else:
+    print(f"y = {a ** 3 + b ** 2}")
