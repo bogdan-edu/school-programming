7d22b030ebcbfd3ce567f12fa4eacad6c48ac861
bogdan-edu <bogdan-education@gmail.com>
2021-10-07 09:54:03 +0300

    fix

--- 2021-2022/11class/deposit_v2.py
+++ 2021-2022/11class/deposit_v2.py
@@ -7,6 +7,6 @@ years = 0
 while deposit < main_sum:
     years = years + 1
     deposit = deposit + percent * deposit / 100
-    print('Через ', years,  'років депозит буде ', deposit)
+    print('Через ', years,  ' років депозит буде ', deposit)
 
 print('Необхідної суми ми досягнемо через ', years, ' років')
