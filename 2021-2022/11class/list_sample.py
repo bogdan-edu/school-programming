0302b6ff13a46fb24439205ec271efa78973f0a8
bogdan-edu <bogdan-education@gmail.com>
2021-10-11 12:03:00 +0300

    add new

--- 2021-2022/11class/list_sample.py
+++ 2021-2022/11class/list_sample.py
@@ -1,5 +1,14 @@
 from random import randint
 
-random_numbers = [ randint(1, 10) for _ in range(20)]
+random_numbers = [randint(1, 10) for _ in range(20)]
 print(random_numbers)
 
+# variant 1
+print('Var1')
+for number in random_numbers:
+    print(number)
+
+# variant 2
+print('Var2')
+for i in range(len(random_numbers)):
+    print(random_numbers[i])
