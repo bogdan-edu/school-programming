6454be207625c5d4d3b2e36a6de4608e799450a1
bogdan-edu <bogdan-education@gmail.com>
2021-11-11 09:44:40 +0200

    add new

--- 2021-2022/11class/find_and_sort/simple_sort_revert.py
+++ 2021-2022/11class/find_and_sort/simple_sort_revert.py
@@ -2,11 +2,11 @@ my_list = [10, 100, 100, 8, 2, -100, 45, -1, -10, 5]
 
 sorted_list = []
 for i in range(len(my_list)):
-    min_el = my_list[0]
+    max_el = my_list[0]
     for el in my_list:
-        if el < min_el:
-            min_el = el
-    sorted_list.append(min_el)
-    my_list.remove(min_el)
+        if el > max_el:
+            max_el = el
+    sorted_list.append(max_el)
+    my_list.remove(max_el)
 print(sorted_list)
 print(my_list)
--- 2021-2022/11class/find_and_sort/simple_sort_without_modify_list_v4.py
+++ 2021-2022/11class/find_and_sort/simple_sort_without_modify_list_v4.py
@@ -1,12 +1,15 @@
 my_list = [10, 100, 100, 8, 2, -100, 45, -1, -10, 5]
 
 sorted_list = []
-for i in range(len(my_list)):
-    min_el = my_list[0]
-    for el in my_list:
+
+temp_list = my_list.copy()
+
+for i in range(len(temp_list)):
+    min_el = temp_list[0]
+    for el in temp_list:
         if el < min_el:
             min_el = el
     sorted_list.append(min_el)
-    my_list.remove(min_el)
+    temp_list.remove(min_el)
 print(sorted_list)
 print(my_list)
