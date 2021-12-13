95190949f0878968d147d413bb88fb53323933a2
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2021-12-13 11:31:26 +0200

    up

---
+++ 2021-2022/11class/graphs/sample_graph_8_6.py
@@ -0,0 +1,11 @@
+graph = [0, 0, 0, 0, 0, 0]
+
+graph[0] = [0, 1, 0, 0, 1, 0]
+graph[1] = [1, 0, 1, 0, 1, 0]
+graph[2] = [0, 1, 0, 1, 0, 0]
+graph[3] = [0, 0, 1, 0, 1, 1]
+graph[4] = [1, 1, 0, 1, 0, 0]
+graph[5] = [0, 0, 0, 1, 0, 0]
+
+for row in range(len(graph)):
+    print(graph[row])
