ab9a410ca56dc5e434a7e7e1449691b8d0008447
bogdan-edu <bogdan-education@gmail.com>
2021-12-14 11:07:43 +0200

    upd

---
+++ 2021-2022/11class/graphs/input_graph.py
@@ -0,0 +1,12 @@
+graph = []
+
+node_len = int(input('Ведіть кількість вершин графа: '))
+
+for i in range(node_len):
+    graph.append([])
+    for j in range(node_len):
+        relation = int(input(f'Чи має вершина {i + 1} звʼязок з вершиною {j + 1} (так - 1, ні - 0) : '))
+        graph[i].append(relation)
+
+for row in range(len(graph)):
+    print(graph[row])
