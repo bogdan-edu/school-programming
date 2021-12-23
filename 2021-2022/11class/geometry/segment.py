1b759b532fb0f57cfdaac831642193b14dfc6b17
bogdan-edu <bogdan-education@gmail.com>
2021-12-23 10:00:33 +0200

    upd

--- 2021-2022/11class/geometry/segment.py
+++ 2021-2022/11class/geometry/segment.py
@@ -1,9 +1,14 @@
-node_a = 1, 1
-node_b = 4, 5
+xa = int(input('Введіть координату x вершини А: '))
+ya = int(input('Введіть координату y вершини B: '))
+xb = int(input('Введіть координату x вершини А: '))
+yb = int(input('Введіть координату y вершини B: '))
+
+node_a = xa, ya
+node_b = xb, yb
 
 
 def long(a, b):
     return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
 
 
-print(long(node_a, node_a))
+print(f'Відстань між точками {node_a} та {node_b} дорівнює {long(node_a, node_b)}')
